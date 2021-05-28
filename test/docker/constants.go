02bbd497e7aa55e8581d52b1bb5eaf9debf844ef
Martin Kysel <mkysel@nuodb.com>
2021-05-28 09:37:41 -0400

    add new docker tests (#31)

---
+++ test/docker/constants.go
@@ -0,0 +1,8 @@
+package docker
+
+const INFLUX_VERSION = "influxdb:1.8"
+const GRAFANA_VERSION = "grafana/grafana:7.5.4"
+
+const ADMIN_CONTAINER_NAME = "nuoadmin1"
+const INFLUXDB_CONTAINER_NAME = "influxdb"
+
