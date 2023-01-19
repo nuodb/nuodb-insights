4353ac79d9567c10e6b548bb1646d4db974cf988
Adrian Suarez <3846392+adriansuarez@users.noreply.github.com>
2023-01-19 13:13:38 -0500

    Bump versions of all dependencies (#37)
    - Bump versions of Grafana and InfluxDB Helm charts. The latest version
      of Grafana suppresses creation of the PodSecurityPolicy resource if it
      is not available.
    - Replace requirements.lock with Chart.lock. Chart.lock is what gets
      generated when dependencies are defined using Chart.yaml. There is no
      requirements.yaml, so requirements.lock should not exist.
    - Update versions of dependencies installed in install_deps.sh.
    - Update install_deps.sh to use latest minikube and to not use
      --vm-driver, which apparently does not (or no longer) accepts --cpus
      and --memory.
    - Use large VM instance, which has 4 cores.
    - Update Golang libraries. All dependencies are inherited through the
      nuodb-helm-charts test library, so go.mod was updated to bump the
      nuodb-helm-charts version and all other libraries there were deleted,
      and then 'go mod tidy' was invoked to update go.mod and go.sum.

--- scripts/ci/install_deps.sh
+++ scripts/ci/install_deps.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
 # exit when any command fails
-set -e
+set -ex
 
 if [[ $TEST_SUITE = "Kubernetes"  ]]; then
+  # conntrack is required by Minikube
+  sudo apt-get update
+  sudo apt-get install -y conntrack
+
   # Download kubectl, which is a requirement for using minikube.
   curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v"${KUBERNETES_VERSION}"/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
-  # Download Helm and Tiller
+  # Download Helm
   wget https://get.helm.sh/helm-"${HELM_VERSION}"-linux-amd64.tar.gz -O /tmp/helm.tar.gz
   tar xzf /tmp/helm.tar.gz -C /tmp --strip-components=1 && chmod +x /tmp/helm && sudo mv /tmp/helm /usr/local/bin
 
   # Download minikube.
   curl -Lo minikube https://storage.googleapis.com/minikube/releases/v"${MINIKUBE_VERSION}"/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-  mkdir -p "$HOME"/.kube "$HOME"/.minikube
-  touch "$KUBECONFIG"
 
   # start minikube
-  sudo minikube start --vm-driver=none --kubernetes-version=v"${KUBERNETES_VERSION}" --memory=8000 --cpus=4
-  sudo chown -R travis: /home/travis/.minikube/
+  minikube start --kubernetes-version=v"${KUBERNETES_VERSION}" --memory=max --cpus=max
+  minikube status
   kubectl cluster-info
 
   helm version
