14d906e1d73fe8c59033afa370b888b10e257f38
AGARWAL Harsh Mohan (INTERN) <HAL5@dsone.3ds.com>
2023-08-02 11:09:04 -0400

    Go files formatting, combined filter func, shrad group added

--- deploy/initdb.sh
+++ deploy/initdb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-influx bucket create --name nuodb_internal --retention 365d
+influx bucket create --name nuodb_internal --retention 365d --shard-group-duration 1d
 
-influx bucket create --name nuodb --retention 365d
+influx bucket create --name nuodb --retention 365d --shard-group-duration 1d
