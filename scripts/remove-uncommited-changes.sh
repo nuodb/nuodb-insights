5c95fae692ec56dad376a748de79fa5041c1f26d
Harsh Mohan Agarwal <hal5@3ds.com>
2023-10-04 12:47:23 -0400

    Remove uncommited changes scripts updated

--- scripts/remove-uncommited-changes.sh
+++ scripts/remove-uncommited-changes.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
 set -e
-GIT_STATUS="$(git status --procelain)"
-VERSION="$(./release/relman.py --check-current --show-head-version)"
-if [ "$GIT_STATUS" != "" ]
-then
-    git checkout -f -- "./stable/insights/Chart.lock"
-fi
+
+git checkout -f -- "./stable/insights/Chart.lock"
